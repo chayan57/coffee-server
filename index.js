07583bc1eed4a97f8970de7e536b5016d19a6c33
chayan57 <ckb8583@gmail.com>
2025-10-17 20:22:03 +0600

    .gitignore uri

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const express = require('express');
 
 
 
-const uri = `mongodb+srv://${process.env.userId}:${process.env.Password}@cluster0.yx95fgn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yx95fgn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
